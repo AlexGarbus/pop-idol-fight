07232f43ec5b6f81959f4f5d5377b63a5161b7fa
AlexGarbus <alex.garbus@outlook.com>
2022-04-06 21:16:19 -0400

    Jump state

--- players/player_state_machine.gd
+++ players/player_state_machine.gd
@@ -4,10 +4,22 @@ extends StateMachine
 
 onready var _idle = $Idle
 onready var _move = $Move
+onready var _jump = $Jump
 
 
 func _ready() -> void:
 	_states_map = {
 		"idle": _idle,
 		"move": _move,
+		"jump": _jump
 	}
+
+
+func _change_state(state_name: String):
+	if not _active:
+		return
+		
+	if state_name in ["jump"]:
+		_states_stack.push_front(_states_map[state_name])
+	
+	._change_state(state_name)
--- players/states/motion/on_ground/on_ground.gd
+++ players/states/motion/on_ground/on_ground.gd
@@ -4,3 +4,9 @@ extends Motion
 
 var _speed := 0.0
 var _velocity := Vector2.ZERO
+
+
+func _handle_input(event: InputEvent) -> void:
+	if event.is_action_pressed("jump" + _player.input_suffix):
+		emit_signal("finished", "jump")
+	._handle_input(event)
