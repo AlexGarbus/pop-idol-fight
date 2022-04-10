002a4a0cabfb7620ec94cf8873c22f8c0d48bbbf
AlexGarbus <alex.garbus@outlook.com>
2022-04-10 14:55:56 -0400

    Jump interrupt

--- players/states/motion/in_air/jump.gd
+++ players/states/motion/in_air/jump.gd
@@ -3,7 +3,17 @@ extends InAir
 
 
 export var jump_force := 1000.0
+export var interrupt_factor := 0.5
+
+var _interrupted:= false
 
 
 func _enter() -> void:
 	_velocity = Vector2(0, -jump_force)
+	_interrupted = false
+
+
+func _handle_input(event: InputEvent) -> void:
+	if _velocity.y < 0 and event.is_action_released("jump" + _player.input_suffix):
+		_interrupted = true
+		_velocity.y *= 0.5
