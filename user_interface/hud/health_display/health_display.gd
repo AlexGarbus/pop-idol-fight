6054a066afa9554173c701ea0449dbbdc17002d2
AlexGarbus <alex.garbus@outlook.com>
2022-04-10 20:41:57 -0400

    Health display reads player names

--- user_interface/hud/health_display/health_display.gd
+++ user_interface/hud/health_display/health_display.gd
@@ -2,6 +2,11 @@ extends Control
 
 
 onready var _progress_bar := $VBoxContainer/ProgressBar
+onready var _name_label := $VBoxContainer/Label
+
+
+func set_name(value: String) -> void:
+	_name_label.text = value
 
 
 func set_health(value: int) -> void:
